bc0bfba8d98f845ac2e8cef749c4029206dde398
aquilax <evgeniy@wikia-inc.com>
2014-11-24 15:51:02 +0100

    UserId

--- api.go
+++ api.go
@@ -16,7 +16,7 @@ type APINode struct {
 	Carbohydrate float64   `json:"carbohydrate"`
 	Protein      float64   `json:"protein"`
 	Barcode      string    `json:"barcode,omitempty"`
-	UserID       string    `json:"-"`
+	UserId       string    `json:"-"`
 	Created      time.Time `json:"created"`
 }
 
