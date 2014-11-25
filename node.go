ea6de544d4574ffd457cdd2d7b82048338d57618
aquilax <evgeniy@wikia-inc.com>
2014-11-25 11:21:44 +0100

    Added nodes and elements to shared

---
+++ node.go
@@ -0,0 +1,27 @@
+package shared
+
+// Node contains general node data
+type Node struct {
+	header   string
+	elements *Elements
+}
+
+// NewNode creates new geneal node
+func NewNode(header string) *Node {
+	return &Node{
+		header,
+		NewElements(),
+	}
+}
+
+// NodeList contains list of general nodes
+type NodeList map[string]*Node
+
+// NewNodeList creates new list of general nodes
+func NewNodeList() *NodeList {
+	return &NodeList{}
+}
+
+func (db *NodeList) push(node *Node) {
+	(*db)[(*node).header] = node
+}
